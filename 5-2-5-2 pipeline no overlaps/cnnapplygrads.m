a9ecfd082ad33e6c796ad9caa2428220aebc0b8c
bhavya6187 <bhavya.6187@gmail.com>
2014-06-23 23:58:10 -0700

    updated weight change rule

---
+++ 5-2-5-2 pipeline no overlaps/cnnapplygrads.m
@@ -0,0 +1,20 @@
+function net = cnnapplygrads(net, opts)
+
+for j = 1 : 16
+    for ii = 1 : 3     
+        net.param1(:,:,ii,j) = net.param1(:,:,ii,j) + opts.alpha*net.layers{1}.dk{ii}{j};
+    end
+    net.b1(j) = net.b1(j) + opts.alpha * net.layers{1}.db{j};
+end
+
+for j = 1 : 16
+    for ii = 1 : 3     
+        net.param2(:,:,ii,j) = net.param2(:,:,ii,j) + opts.alpha*net.layers{3}.dk{ii}{j};
+    end
+    net.b2(j) = net.b2(j) + opts.alpha * net.layers{3}.db{j};
+end
+
+net.ffW = net.ffW + opts.alpha * net.dffW;
+net.ffb = net.ffb + opts.alpha * net.dffb;
+
+end
---
+++ 5-2-5-2 pipeline no overlaps/cnnff.m
@@ -0,0 +1,52 @@
+function net = cnnff(net,x,y)
+
+%x = padarray(x,[2 2],'both');
+
+for j = 1:16
+    z = zeros(28,28,1,size(x,4));
+    for i = 1 : 3
+        z = z + convn(x(:,:,i,:),net.param1(:,:,i,j),'valid');
+    end
+    net.layers{1}.a{j} = sigm(z + net.b1(j));
+    net.layers{1}.a{j} = squeeze(net.layers{1}.a{j});
+end
+
+net.fv = [];
+
+for j = 1:16
+    z = convn(net.layers{1}.a{j}, ones(2) / (4), 'valid');   %  !! replace with variable
+    net.layers{2}.a{j} = z(1 : 2 : end, 1 : 2 : end, :);
+end
+
+for j = 1:16
+    z = zeros(10,10,size(x,4));
+    for i = 1 : 3
+        z = z + convn(net.layers{2}.a{i},net.param2(:,:,i,j),'valid');
+    end
+    net.layers{3}.a{j} = sigm(z + net.b2(j));
+end
+
+net.fv = [];
+
+for j = 1:16
+    z = convn(net.layers{3}.a{j}, ones(2) / (4), 'valid');   %  !! replace with variable
+    net.layers{4}.a{j} = z(1 : 2 : end, 1 : 2 : end, :);
+end
+
+
+for j = 1 : numel(net.layers{4}.a)
+    sa = size(net.layers{4}.a{j});
+    net.fv = [net.fv; reshape(net.layers{4}.a{j}, sa(1) * sa(2), sa(3))];
+end
+
+net.o = sigm(net.ffW * net.fv + repmat(net.ffb, 1, size(net.fv, 2)));
+
+result = double(bsxfun(@eq, net.o, max(net.o, [], 1)));
+net.errors = 0;
+
+for i = 1:size(y,2)
+    er = ~all(y(:,i)==result(:,i));
+    net.errors = net.errors+er;
+end
+
+end
