e905f229bfbc66ff2e0ddb7573409d10d1a94dd0
bhavya6187 <bhavya.6187@gmail.com>
2014-10-01 03:40:39 -0400

    syncing

--- Pre Trained model/cnnTrain.m
+++ Pre Trained model/cnnTrain.m
@@ -4,18 +4,16 @@ load '../data/input.mat';
 
 train_y = train_y';
 test_y = test_y';
-
-opts.alpha = 1;
-opts.batchsize = 50;
-opts.numepochs = 2;
+train_x = train_x/255;
+test_x = test_x/255;
 
 M = csvread('conv1.csv');
 Mb = csvread('conv1_biases.csv');
 
 for j = 1 : 32 %  output map
-    net.param1{1}{j} = reshape(M(1:25,j),5,[]);
-    net.param1{2}{j} = reshape(M(26:50,j),5,[]);
-    net.param1{3}{j} = reshape(M(51:75,j),5,[]);
+    net.param1{1}{j} = reshape(M(1:25,j),5,[])';
+    net.param1{2}{j} = reshape(M(26:50,j),5,[])';
+    net.param1{3}{j} = reshape(M(51:75,j),5,[])';
     net.b1{j} = Mb(j);
 end
 
@@ -24,21 +22,13 @@ Nb = csvread('conv2_biases.csv');
 
 for j = 1 : 32 %  output map
     for i = 1 : 32 %  input map
-        net.param2{i}{j} = reshape(N(25*(i-1)+1:25*i,j),5,[]);
+        net.param2{i}{j} = reshape(N(25*(i-1)+1:25*i,j),5,[])';
     end
     net.b2{j} = Nb(j);
 end
 
 net.ffW = csvread('fc10.csv')';
 net.ffb = csvread('fc10_biases.csv')';
-
-net = cnnff(net, train_x,train_y);
-
-result = double(bsxfun(@eq, net.o, max(net.o, [], 1)));
-errors = 0;
-for i = 1:size(test_y,2)
-    er = ~all(test_y(:,i)==result(:,i));
-    errors = errors+er;
-end
-disp(errors)
-%er = numel(bad) / size(test_y, 2);
+size(test_x)
+size(test_y)
+net = cnnff(net, test_x(:,:,:,1:100),test_y(:,1:100));
