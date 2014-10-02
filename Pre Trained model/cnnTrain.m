e0a8e4a7c785d942c170ce78cd2053c15172c774
bhavya6187 <bhavya.6187@gmail.com>
2014-10-02 15:37:44 -0400

    experimenting with transpose

--- Pre Trained model/cnnTrain.m
+++ Pre Trained model/cnnTrain.m
@@ -11,9 +11,9 @@ M = csvread('conv1.csv');
 Mb = csvread('conv1_biases.csv');
 
 for j = 1 : 32 %  output map
-    net.param1{1}{j} = reshape(M(1:25,j),5,[])';
-    net.param1{2}{j} = reshape(M(26:50,j),5,[])';
-    net.param1{3}{j} = reshape(M(51:75,j),5,[])';
+    net.param1{1}{j} = reshape(M(1:25,j),5,[]);
+    net.param1{2}{j} = reshape(M(26:50,j),5,[]);
+    net.param1{3}{j} = reshape(M(51:75,j),5,[]);
     net.b1{j} = Mb(j);
 end
 
@@ -22,7 +22,7 @@ Nb = csvread('conv2_biases.csv');
 
 for j = 1 : 32 %  output map
     for i = 1 : 32 %  input map
-        net.param2{i}{j} = reshape(N(25*(i-1)+1:25*i,j),5,[])';
+        net.param2{i}{j} = reshape(N(25*(i-1)+1:25*i,j),5,[]);
     end
     net.b2{j} = Nb(j);
 end
